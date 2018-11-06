addc14f2e4a4d09a44e418f06feff3974f1786ae
Sam Pryor <spryor84@gmail.com>
2018-11-06 15:06:08 -0500

    seedsfinal

--- db/seeds/tvshow.sql
+++ db/seeds/tvshow.sql
@@ -2,16 +2,16 @@
 
 INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Friends','Comedy','NBC','https://image.tmdb.org/t/p/w500/efiX8iir6GEBWCD0uCFIi5NAyYA.jpg','46.572');
 INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Game of Thrones','Sci-Fi & Fantasy, Drama, Action & Adventure','HBO','https://image.tmdb.org/t/p/w500/gX8SYlnL9ZznfZwEH4KJUePBFUM.jpg','53.742');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Seinfeld','','','https://image.tmdb.org/t/p/w500/8JH8s4WKKSxGxtJvWbQAdaEjv9q.jpg','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Cheers','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Everybody Loves Raymond','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Simpsons','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Sopranos','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Breaking Bad','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Wire','','','https://image.tmdb.org/t/p/w500','');
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Mad Men','','','https://image.tmdb.org/t/p/w500','');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Seinfeld','Comedy','NBC','https://image.tmdb.org/t/p/w500/8JH8s4WKKSxGxtJvWbQAdaEjv9q.jpg','21.773');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Cheers','Comedy','NBC','https://image.tmdb.org/t/p/w500','13.279');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Everybody Loves Raymond','Comedy','CBS','https://image.tmdb.org/t/p/w500/txiVDIgM0WVCBhlOdPiyPhCB1Va.jpg','16.854');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Simpsons','Animation, Comedy','FOX','https://image.tmdb.org/t/p/w500/lnnrirKFGwFW18GiH3AmuYy40cz.jpg','148.443');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Sopranos','Drama','HBO','https://image.tmdb.org/t/p/w500/8GZ91vtbYOMp05qruAGPezWC0Ja.jpg','19.032');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Breaking Bad','Drama','AMC','https://image.tmdb.org/t/p/w500/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg','39.564');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('The Wire','Drama, Crime','HBO','https://image.tmdb.org/t/p/w500/4hWfYN3wiOZZXC7t6B70BF9iUvk.jpg','25.723');
+INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('Mad Men','Drama','AMC','https://image.tmdb.org/t/p/w500/jLYR42fOcE3balZ9svvJM0Rus91.jpg','15.658');
 
 
 
-INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('','','','','');
+-- INSERT INTO tvshow(flavor,description,rating,url,brand) VALUES ('','','','','');
 
