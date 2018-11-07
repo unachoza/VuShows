e471d8476819b864067f9e2782b9fbd3a68be603
Arleen Santana <santana.arleen@gmail.com>
2018-11-07 09:47:59 -0500

    updated tvshowlist structure; css not complete; button not linked'

--- client/src/components/TvShow.js
+++ client/src/components/TvShow.js
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 const TvShow = (props) => {
   return (
     <div className="tv-inlist">
-      <img alt='' src={props.tvshow.url} />
-      <h2>{props.tvshow.flavor}</h2>
-      <p>Rating: {props.tvshow.rating || 'N/A'}</p>
-      <Link to={`/tv-show/${props.tvshow.id}`}>See More</Link>
-    </div>
+  		<div class="backgroundimage"></div>
+  		<div class="show-heading">
+  			<div class="show-poster"></div>
+   			<div class="info">
+   			<h2>Friends</h2>
+     	</div>
+  	</div>
   )
 }
 
