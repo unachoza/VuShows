f55e84e3ff09f3b17094a1d9bd5f3650df95df82
mpkozak <mparkerkozak@gmail.com>
2018-11-09 10:02:05 -0500

    server updated, integrated

--- db/config.js
+++ db/config.js
@@ -9,7 +9,7 @@ const pgp = require('pg-promise')(options);
 function setDatabase() {
   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     return pgp({
-      database: 'tvshow_dev',
+      database: 'crypt_db',
       port: 5432,
       host: 'localhost',
     })
